41c914a06746ed37b91e124076329f4d7e8ea685
chylomicronman <bmccann36@gmail.com>
2020-11-22 19:27:29 -0500

    made tweaks to the main prompt script

--- scratch.ts
+++ scratch.ts
@@ -1,3 +1,14 @@
 import * as moment from 'moment';
+import * as fs from "fs";
+import * as path from "path";
+import { Remarkable } from 'remarkable-typescript';
+// console.log(moment().format('Z')); // get timezone
 
-console.log(moment().format('Z'));
+const token = fs.readFileSync(path.join(__dirname, "./userData/deviceToken.txt")).toString();
+const client = new Remarkable({deviceToken: token});
+(async()=> {
+  await client.refreshToken();
+ 
+  const items = await client.getAllItems();
+  console.log(items);
+})()
--- src/setupUser/main.ts
+++ src/setupUser/main.ts
@@ -7,39 +7,76 @@ import {
   NlMap,
 } from "../commonModels/NewsletterData";
 import { newsletterMap } from "../../static/newsletters";
+import * as path from "path";
+import { stdout } from "process";
+import * as chalk from "chalk";
 
-captureToken()
-.then(() => capturePreferences());
-// capturePreferences()
+const userDataDir = path.join(process.cwd(), "userData");
+const oldTokenExists = checkForExistingToken();
+const rmClient = new Remarkable();
 
-async function captureToken() {
-  const rmClient = new Remarkable();
-
-
-  const response = await prompts([
+(async () => {
+  const promptOutputs01 = await prompts([
     {
-      type: "confirm",
-      name: "value",
+      type: () => (oldTokenExists ? "confirm" : null), // determines if prompt shoudl be shown
+      name: "should-overwrite-token",
       message:
-        `- please login to your account at https://my.remarkable.com/ 
-         - Once logged in, click on the link under "Browser extension" to manage your connected apps
-         - Once you have the code hit enter or type "y" to continue`,
+        "it appears you've already authorized this app to write to your Remarkable, would you like to delete the existing credentials and generate new ones?",
+    },
+    {
+      type: (prev) => {
+        // only present this promt if a) no token exists b) user wants to overwrite the old one
+        if (prev == undefined || prev == true) return "confirm";
+      },
+      name: "will-generate-new-token",
+      message: setupTokenGeneration,
       initial: true,
     },
     {
       type: "text",
-      name: "pairingCode",
-      message: "enter the code",
+      name: "prompt-code-entry",
+      message: "please enter the code that was generated",
     },
   ]);
-  // const deviceToken = await rmClient.register({ code: "bdnfmdzb" }); //! not doing for now
-  console.log("response :>> ", response);
-  console.log("simulating a token fetch");
-  await sleep(1000);
-  const fakeToken = "abcdefsFAKE-TOKEN";
+  const code = promptOutputs01["prompt-code-entry"];
+  await getAndSaveToken(code);
 
+})();
+
+function checkForExistingToken() {
+  let tokenExists = false;
+  try {
+    const tokenFileData = fs.statSync(userDataDir + "/deviceToken.txt");
+    if (tokenFileData) {
+      tokenExists = true;
+    }
+  } catch (ex) {
+    console.log(
+      chalk.green("no token detected, will prompt to create a new device token")
+    );
+  }
+  return tokenExists;
+}
+
+function setupTokenGeneration(prev, values) {
+  // delete the old token
+  if (oldTokenExists) {
+    fs.unlinkSync(userDataDir + "/deviceToken.txt");
+  }
+  return `**FIRST STEPS**
+  - please login to your account at https://my.remarkable.com/
+  - Once logged in, click on the link under "Browser extension" or navigate to https://my.remarkable.com/list/browser to manage your connected apps
+  - Click "Connect new broswer" to generate a one time code
+  - Once you have copied the code hit enter or type "y" to continue`;
+}
+
+
+async function getAndSaveToken(code: string) {
+  console.log(chalk.green("using code to register remarkable-times with your device..."))
+
+  const deviceToken = await rmClient.register({ code: code });
   // ? write token to userData directory
-  fs.writeFileSync("./userData/deviceToken.txt", fakeToken);
+  fs.writeFileSync("./userData/deviceToken.txt", deviceToken);
 }
 
 async function capturePreferences() {
@@ -62,7 +99,7 @@ async function capturePreferences() {
   ]);
   // console.log('promptSection2 :>> ', nlSelection);
   const preferenceAsJson = JSON.stringify(nlSelection.selectedNewsletters);
-  fs.writeFileSync("./userData/nlPreferences.json", preferenceAsJson)
+  fs.writeFileSync("./userData/nlPreferences.json", preferenceAsJson);
 }
 
 function sleep(ms) {

f9fe1c30236a3caf4e0add5441bbd0cccd6ddd40
chylomicronman <bmccann36@gmail.com>
2020-12-10 11:27:39 -0500

    made log setup configurable with env vars
    create more flexible implementation to remove font size attribute

--- src/fetchNlContent/reformatNlHtml.ts
+++ src/fetchNlContent/reformatNlHtml.ts
@@ -1,40 +1,41 @@
-import * as cheerio from "cheerio";
-import * as fs from "fs";
-import * as path from "path";
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const reformatNlHtml = function (newsLetterHtml) {
+const reformatNlHtml = function (newsLetterHtml: string, title?: string) {
   // load data into cheerio parser
   const $ = cheerio.load(newsLetterHtml);
 
   // remove div styling
-  $("div").each(function (i, el) {
-    $(el).attr("style", null);
+  $('div').each(function (i, el) {
+    $(el).attr('style', null);
   });
 
   /**
    * iterates over each tbody element and removes the styling of child elements within the tbody
    */
-  $("tbody").each(function (i, el) {
+  $('tbody').each(function (i, el) {
     // select any "span" element that is inside a tbody element
-    $(el).find("span").attr("style", null);
+    $(el).find('span').attr('style', null);
 
-    const tdStyle = $(el).find("td").attr("style");
-
+    const tdStyle = $(el).find('td').attr('style');
     // remove font size
     if (tdStyle) {
-      const newTdStyle = tdStyle.replace("font-size:0;", "");
-      $(el).find("td").attr("style", newTdStyle);
+      const newTdStyle = removeFontStyles(tdStyle);
+      $(el).find('td').attr('style', newTdStyle);
     }
-    $(el).find("p").attr("style", null);
-    const ulSpec = $(el).find("ul").attr("style");
+    // set paragraph styles to null
+    $(el).find('p').attr('style', null);
+    const ulSpec = $(el).find('ul').attr('style');
     if (ulSpec) {
-      const newUlSpec = ulSpec.replace("font:10px georgia,serif;", "");
-      $(el).find("ul").attr("style", newUlSpec);
+      const newUlSpec = removeFontStyles(ulSpec);
+      $(el).find('ul').attr('style', newUlSpec);
     }
   });
+
   const emailContent = $('td[id="EMAIL_CONTAINER"]');
   //? for when you want to write to file
-  // fs.writeFileSync("./nlHtml.html", emailContent.html());
+  fs.writeFileSync(path.join(__dirname, '..', '..', `nlHtml/${title}.html`), emailContent.html());
   return emailContent.html();
 };
 
@@ -44,3 +45,16 @@ export default reformatNlHtml;
 // const buffData = fs.readFileSync(path.join(__dirname, "..", "..", "fetchedPages/Morning Briefing.html"))
 // const morningBriefingString = buffData.toString();
 // reformatNlHtml(morningBriefingString)
+
+/**
+ *
+ * @param styleAttributes string of inline style attributes
+ * @returns { filteredStyleAttributes } same style attributes but with any font style removed
+ */
+function removeFontStyles(styleAttributes: string): string {
+  const attrList = styleAttributes.split(';');
+  const filtered = attrList.filter((cssProp) => {
+    return cssProp.includes('font') == false;
+  });
+  return filtered.join();
+}
--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,15 @@
-import { ILogObject, IStd, Logger } from "tslog";
-import { appendFileSync } from "fs";
-import * as path from "path";
+import dotenv from 'dotenv';
+dotenv.config();
+import { ILogObject, IStd, Logger, TLogLevelName } from 'tslog';
+import { appendFileSync } from 'fs';
+import * as path from 'path';
 
 /**
  * simple utility class for tslog to set internal buffer object
  * stole this from the docs https://tslog.js.org/#/?id=printprettylog
  */
 class SimpleStd implements IStd {
-  constructor(private _buffer: string = "") {}
+  constructor(private _buffer: string = '') {}
   write(message: string) {
     this._buffer += message;
   }
@@ -16,11 +18,15 @@ class SimpleStd implements IStd {
   }
 }
 
+// coerce string value into boolean
+const shouldPipeLogs: boolean = process.env.PIPE_CONSOLE_INTO_LOG_FILE == 'true' ? true : false;
+const shouldColorize: boolean = process.env.LOG_COLORIZE == 'true' ? true : false;
+
 const log: Logger = new Logger(
   /* specify console overwrite sot we capture console.log by 3rd party libs */ {
-    overwriteConsole: true,
-    minLevel: "info",
-    colorizePrettyLogs: false // good for log files
+    overwriteConsole: process.env.PIPE_CONSOLE_INTO_LOG_FILE ? shouldPipeLogs : true,
+    minLevel: <TLogLevelName>process.env.LOG_MIN_LEVEL,
+    colorizePrettyLogs: process.env.LOG_COLORIZE ? shouldColorize : false, // good for log files
   }
 );
 log.attachTransport(
@@ -33,14 +39,14 @@ log.attachTransport(
     error: logToTransport,
     fatal: logToTransport,
   },
-  "debug"
+  'debug'
 );
 
 /**
  * every day write logs to a new file to keep em separate
  */
-const dateString = new Date().toISOString().split("T")[0];
-const logDir = path.join(__dirname, "..", "logs");
+const dateString = new Date().toISOString().split('T')[0];
+const logDir = path.join(__dirname, '..', 'logs');
 const todayLogFileName = logDir + `/${dateString}.log`;
 
 function logToTransport(logObject: ILogObject) {
